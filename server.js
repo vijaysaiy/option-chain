0af58955bae9865b12b96164eb67657e953ce99a
Naveen Jerubandi <naveen.jerubandi@tv2z.com>
2022-07-16 23:29:43 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const corsOpts = {
 };
 app.use(cors(corsOpts));
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "https://tradingdata.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
