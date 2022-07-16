febd43076ec11d0109c4474715f2dc753252376b
Naveen Jerubandi <naveen.jerubandi@tv2z.com>
2022-07-16 21:48:22 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ const corsOpts = {
   allowedHeaders: ["Content-Type"],
 };
 app.use(cors(corsOpts));
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
 app.use(morgan("dev"));
 
