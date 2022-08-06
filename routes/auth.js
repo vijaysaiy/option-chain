f5192058c7c30801996b2f470007a0f5fc122fd4
palki shiva kumar <shivakumarpalki@gmail.com>
2022-08-06 15:57:04 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { register, login } from "../controllers/auth.js";
+import { testnifty } from "../controllers/nse.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", register);
+authRouter.post("/register", testnifty);
 authRouter.post("/login", login);
 
 export default authRouter;
