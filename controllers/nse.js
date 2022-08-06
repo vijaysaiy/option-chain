147d1dceb4ce512c3f5d10febf3550ea2572f26d
palki shiva kumar <shivakumarpalki@gmail.com>
2022-08-06 16:51:37 +0530

    h

--- controllers/nse.js
+++ controllers/nse.js
@@ -4,7 +4,7 @@ export const bankNifty = async (req, res) => {
   try {
     const bankNiftyData = await axios.get(process.env.BANKNIFTY_API);
     // return res.json(bankNiftyData.data);
-    return res.status(200).json("tru7e");
+    return res.status(200).json(axios);
   } catch (error) {
     console.log("ERROR IN BANKNIFTY ==>", error);
     return res.status(500).json(error + process.env.BANKNIFTY_API);
