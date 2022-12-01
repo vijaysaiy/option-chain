a913a52163c46084f8ddedbb115cfee8b78ae6db
Vijaysai Yekbote <vijaysaiyekbote@gmail.com>
2022-12-01 20:32:22 +0530

    Changes to cors

--- server.js
+++ server.js
@@ -19,30 +19,20 @@ mongoose
   .catch((err) => console.log("error in database connection", err));
 
 //middlewares
-const corsOpts = {
+const corsOptions = {
   origin: "*",
-  referer: "*",
-
-  methods: ["GET", "POST"],
-
-  allowedHeaders: ["Content-Type"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
-app.use(cors(corsOpts));
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
 app.use(express.json());
 app.use(morgan("dev"));
 
 //route middleware
 app.use("/api/auth", authRouter);
 app.use("/api/nse", nseRouter);
-app.use(cors(corsOpts));
-
 
 //listening the app on the server on port
 const port = process.env.PORT || 8000;
